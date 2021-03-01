432ec7825f40463bbd838d5d633a8c3cead21fc0
Robert Geifman <robertgeifman@me.com>
2021-03-01 10:55:57 +0100

    edited package manifest

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     	.tvOS(.v13)
 	],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .executable(
         	name: "natalie",
         	targets: ["natalie"]),
@@ -20,19 +19,18 @@ let package = Package(
             targets: ["NatalieSupport"]),
     ],
     dependencies: [
-//        .package(url: "https://github.com/robertgeifman/Aspects.git", .branch("develop")),
-//        .package(url: "https://github.com/robertgeifman/FoundationAdditions.git", .branch("develop")),
-        .package(path: "~/Projects/Packages/Aspects"),
-        .package(path: "~/Projects/Packages/FoundationAdditions")
+        .package(path: "../Aspects"),
+        .package(path: "../FoundationAdditions")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "natalie",
             dependencies: []),
         .target(
             name: "NatalieSupport",
-            dependencies: ["Aspects", "FoundationAdditions"])
+            dependencies: ["FoundationAdditions",
+            	"Aspects",
+			]
+		)
     ]
 )
